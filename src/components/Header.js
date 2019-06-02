5ecc7b74affae86ff73402200173f88bc99178f6
anniebombanie <anniebombanie@gmail.com>
2019-06-01 23:26:37 -0400

    added printing press

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import logo from './../assets/cog.svg';
+import printingPress from './../assets/printingPress.png';
 
 class Header extends Component {
 
   render() {
     return (
       <header>
-      <div className="logo-lines"></div>
-        <img src={logo} className="logo-cog" alt="logo: black cog with spanner cut-out in the middle." />
-        <h1>The Absolutely Amazing Newspaper Apparatus</h1>
-        <p>Hear ye, hear ye! Have you ever dreamt of your very own customised newspaper? The age of the machine has made it possible! Fill in your details and our amazing apparatus will do the magic! Prepared to be awed and astounded!</p>
+        <div className="containerLogo">
+          <img
+            src={logo}
+            className="logoCog"
+            alt="logo: rotating black cog with spanner cut-out in the middle and three lines on either side."
+          />
+        </div>
+        <h1>The Absolutely Amazing Newspaper Apparatus!</h1>
+        <p>Hear ye, hear ye!</p>
+        <p>Have you ever dreamt of your very own customised newspaper? Well, good news: the age of the machine has made it possible!</p>
+        <img src={printingPress} alt='' />
+        <p>Fill in your details and our amazing apparatus will do the magic! Prepared to be awed and astounded!</p>
       </header>
-    )
+    );
   }
 }
 
