c3b718a32153d868925c7fa27286ca26c84a70b2
anniebombanie <anniebombanie@gmail.com>
2019-06-02 17:27:38 -0400

    Updates

---
+++ precache-manifest.ee23e4d2385b4ca71bae8ac89d6fbb09.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8c2c91ded489a8c1dd6e1b6dcef2d0a",
+    "url": "/annie-liew-project-five/index.html"
+  },
+  {
+    "revision": "4f7235c2ac8404c0e5cd",
+    "url": "/annie-liew-project-five/static/css/main.24d14c54.chunk.css"
+  },
+  {
+    "revision": "e56d0da9aefb5b73e918",
+    "url": "/annie-liew-project-five/static/js/2.348e7d6f.chunk.js"
+  },
+  {
+    "revision": "4f7235c2ac8404c0e5cd",
+    "url": "/annie-liew-project-five/static/js/main.240a5880.chunk.js"
+  },
+  {
+    "revision": "7d0407007dc6812acd6b",
+    "url": "/annie-liew-project-five/static/js/runtime~main.a6913a76.js"
+  },
+  {
+    "revision": "cb9a93cfc063f05de27c1126fac45288",
+    "url": "/annie-liew-project-five/static/media/cog.cb9a93cf.svg"
+  },
+  {
+    "revision": "b8e120dbff7542a978dcf1cee94e2796",
+    "url": "/annie-liew-project-five/static/media/logo-lines-l.b8e120db.svg"
+  },
+  {
+    "revision": "9279d80817612d9a54ae2c6db808d568",
+    "url": "/annie-liew-project-five/static/media/logo-lines-r.9279d808.svg"
+  },
+  {
+    "revision": "a091e8782aa195573db1193cb2275e3e",
+    "url": "/annie-liew-project-five/static/media/pointing-finger.a091e878.png"
+  },
+  {
+    "revision": "19c8569d94f78bc40748bbd2f2aa14e9",
+    "url": "/annie-liew-project-five/static/media/printing-press.19c8569d.png"
+  },
+  {
+    "revision": "98154d27487bf2b1144da528b34f4c3b",
+    "url": "/annie-liew-project-five/static/media/soothsaying-ad.98154d27.svg"
+  }
+]);
