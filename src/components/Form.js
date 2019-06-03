06e55ce7b3dadd4fdd0e1241bd370c0829f77ae8
anniebombanie <anniebombanie@gmail.com>
2019-06-03 06:39:12 -0400

    styling form

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,134 +4,161 @@ class Form extends Component {
   render() {
     return (
       <form action="" onSubmit={this.props.submitForm}>
-        <label htmlFor="user-name">What's your full name?</label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          placeholder="Enter your full name here"
-          value={this.props.name}
-          onChange={e => {
-            this.props.handleChange(e);
-          }}
-        />
-        {this.props.errors.name && (
-          <div className="container-error">
-            <p>Uh-oh! The apparatus needs your name to start!</p>
-          </div>
-        )}
-
-        <legend>Where were you born?</legend>
-        <div>
-          <label htmlFor="city">City</label>
+        <div className="container-name">
+          <legend>What's your full name?</legend>
+          <label htmlFor="user-name" className="visually-hidden">
+            Full name
+          </label>
           <input
             type="text"
-            name="city"
-            id="city"
-            placeholder="Enter your City here"
-            value={this.props.city}
+            name="name"
+            id="name"
+            placeholder="Full name"
+            value={this.props.name}
             onChange={e => {
               this.props.handleChange(e);
             }}
           />
+          {this.props.errors.name && (
+            <div className="container-error">
+              <p>Uh-oh! The apparatus needs your name to start!</p>
+            </div>
+          )}
         </div>
-        {this.props.errors.city && (
-          <div className="container-error">
-            <p>Zounds! Please tell the apparatus your city of birth!</p>
-          </div>
-        )}
 
-        <div>
-          <label htmlFor="country">Country</label>
-          <input
-            type="text"
-            name="country"
-            id="country"
-            placeholder="Enter your Country here"
-            value={this.props.country}
-            onChange={e => {
-              this.props.handleChange(e);
-            }}
-          />
-        </div>
-        {this.props.errors.country && (
-          <div className="container-error">
-            <p>Uh-oh.. The apparatus needs to know your country of birth to operate!</p>
+        <div className="container-birthplace">
+          <legend>Where were you born?</legend>
+          <div>
+            <label htmlFor="city" className="visually-hidden">
+              City
+            </label>
+            <input
+              type="text"
+              name="city"
+              id="city"
+              placeholder="City"
+              value={this.props.city}
+              onChange={e => {
+                this.props.handleChange(e);
+              }}
+            />
           </div>
-        )}
+          {this.props.errors.city && (
+            <div className="container-error">
+              <p>Zounds! Please tell the apparatus your city of birth!</p>
+            </div>
+          )}
 
-        <legend>When's your birthday?</legend>
-        <div>
-          <label htmlFor="month">Month</label>
-          <select
-            id="month"
-            name="month"
-            placeholder="month"
-            value={this.props.month}
-            onChange={e => {
-              this.props.handleChange(e);
-            }}
-          >
-            <option value="">Month</option>
-            <option value="1">January</option>
-            <option value="2">February</option>
-            <option value="3">March</option>
-            <option value="4">April</option>
-            <option value="5">May</option>
-            <option value="6">June</option>
-            <option value="7">July</option>
-            <option value="8">August</option>
-            <option value="9">September</option>
-            <option value="10">October</option>
-            <option value="11">November</option>
-            <option value="12">December</option>
-          </select>
-        </div>
-        {this.props.errors.month && (
-          <div className="container-error">
-            <p>Zounds! Your birth month is required to continue!</p>
+          <div>
+            <label htmlFor="country" className="visually-hidden">
+              Country
+            </label>
+            <input
+              type="text"
+              name="country"
+              id="country"
+              placeholder="Country"
+              value={this.props.country}
+              onChange={e => {
+                this.props.handleChange(e);
+              }}
+            />
           </div>
-        )}
+          {this.props.errors.country && (
+            <div className="container-error">
+              <p>
+                {' '}
+                Uh-oh.. The apparatus needs to know your country of birth to
+                operate!
+              </p>
+            </div>
+          )}
+        </div>
 
-        <div>
-          <label htmlFor="day">Day</label>
-          <input
-            type="number"
-            min="1"
-            max="31"
-            id="day"
-            name="day"
-            placeholder="Day"
-            value={this.props.day}
-            onChange={e => {
-              this.props.handleChange(e);
-            }}
-          />
-        </div>
-        {this.props.errors.day && (
-          <div className="container-error">
-            <p>Kindly input your birth day to operate the apparatus!</p>
+        <div className="container-DOB">
+          <legend>When's your birthday?</legend>
+          <div>
+            <label htmlFor="month" className="visually-hidden">
+              Month
+            </label>
+            <select
+              id="month"
+              name="month"
+              placeholder="month"
+              value={this.props.month}
+              onChange={e => {
+                this.props.handleChange(e);
+              }}
+            >
+              <option value="" disabled>
+                Month
+              </option>
+              <option value="1">January</option>
+              <option value="2">February</option>
+              <option value="3">March</option>
+              <option value="4">April</option>
+              <option value="5">May</option>
+              <option value="6">June</option>
+              <option value="7">July</option>
+              <option value="8">August</option>
+              <option value="9">September</option>
+              <option value="10">October</option>
+              <option value="11">November</option>
+              <option value="12">December</option>
+            </select>
           </div>
-        )}
+          {this.props.errors.month && (
+            <div className="container-error">
+              <p>Zounds! Your birth month is required to continue!</p>
+            </div>
+          )}
 
-        <div>
-          <label htmlFor="year">Year</label>
-          <input
-            type="number"
-            id="year"
-            name="year"
-            placeholder="Year"
-            value={this.props.year}
-            onChange={e => {
-              this.props.handleChange(e);
-            }}
-          />
-        </div>
-        {this.props.errors.year && (
-          <div className="container-error">
-            <p>Uh-oh! Please input your birth year to run the apparatus!</p>
+          <div>
+            <label htmlFor="day" className="visually-hidden">
+              Day
+            </label>
+            <input
+              type="number"
+              min="1"
+              max="31"
+              id="day"
+              name="day"
+              placeholder="Day"
+              value={this.props.day}
+              onChange={e => {
+                this.props.handleChange(e);
+              }}
+            />
           </div>
-        )}
+          {this.props.errors.day && (
+            <div className="container-error">
+              <p>Kindly input your birth day to operate the apparatus!</p>
+            </div>
+          )}
+
+          <div>
+            <label htmlFor="year" className="visually-hidden">
+              Year
+            </label>
+            <input
+              type="number"
+              id="year"
+              name="year"
+              placeholder="Year"
+              value={this.props.year}
+              onChange={e => {
+                this.props.handleChange(e);
+              }}
+            />
+          </div>
+          {this.props.errors.year && (
+            <div className="container-error">
+              <p>
+                Uh-oh! Please input your birth year to run the apparatus!
+              </p>
+            </div>
+          )}
+        </div>
 
         <input type="submit" value="Get my Newspaper" />
         {Object.values(this.props.errors).some(err => err) && (
