328506f33fc9cbd543fef5e9e49a69cff25f4f7f
anniebombanie <anniebombanie@gmail.com>
2019-06-03 11:56:10 -0400

    working on final responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,17 +7,41 @@ class Header extends Component {
   render() {
     return (
       <header>
-        <div className='container-logo'>
-          <img src={logo} className='logo-cog' alt='logo: rotating black cog with spanner cut-out in the middle.' />
+        <div className="container-logo">
+          <img
+            src={logo}
+            className="logo-cog"
+            alt="logo: rotating black cog with spanner cut-out in the middle."
+          />
         </div>
         <h1>The Absolutely Amazing Newspaper Apparatus!</h1>
-        <div className='container-line'>
-          <div></div>
+        <div className="container-line">
+          <div />
         </div>
-        <p className='black italic'>Hear ye, hear ye!</p>
-        <p>Have you ever dreamt of your very own customised newspaper? Well, good news; the Age of the Machine has made it possible!</p>
-        <img src={printingPress} alt='illustration of vintage printing press.' />
-        <p>Fill in your details and our world's first amazing apparatus will do its magic! <span className='bold uppercase'>Prepared to be awed and astounded!</span></p>
+        <div className="container-intro-content">
+          <p className="hear-ye black italic">Hear ye, hear ye!</p>
+          <div>
+            <div className="container-txt">
+              <p>
+                Have you ever dreamt of your very own customised newspaper? Well, good news; the Age of the Machine has made it possible!
+              </p>
+            </div>
+            <div className="container-img">
+              <img
+                src={printingPress}
+                alt="illustration of vintage printing press."
+              />
+            </div>
+            <div className="container-txt">
+              <p>
+                Fill in your details and our world's first amazing apparatus will do its magic! 
+                <span className="bold uppercase">
+                   Prepared to be awed and astounded!
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
       </header>
     );
   }
