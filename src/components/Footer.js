8ff3348211924f89571a47d3aab3723189a35766
anniebombanie <anniebombanie@gmail.com>
2019-06-03 22:36:21 -0400

    added rel=noopener

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,11 +6,11 @@ class Footer extends Component {
       <footer>
         <div className="wrapper">
           <p>
-            Designed & hand-coded with ♥ by <a href="http://www.anniebombanie.com" target="_blank" aria-label="go to Annie Liew's website."> Annie Liew</a>
+            Designed & hand-coded with ♥ by <a href="http://www.anniebombanie.com" target="_blank" aria-label="go to Annie Liew's website." rel="noopener">Annie Liew</a> | Powered by the <a href="http://numbersapi.com" target="_blank" aria-label="go to numbers api's website." rel="noopener">Numbers API</a>
           </p>
         </div>
       </footer>
-    )}
+    );}
 }
 
 export default Footer
