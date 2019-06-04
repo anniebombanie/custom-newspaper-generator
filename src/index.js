fbff87d50248df7c337352c060ee432b1662373f
anniebombanie <anniebombanie@gmail.com>
2019-06-04 01:31:42 -0400

    added custom faviconv + small updates

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/App.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import WebFont from 'webfontloader';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/App.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import WebFont from "webfontloader";
 WebFont.load({
   google: {
     families: [
-      'Rye',
-      'cursive',
-      'Vollkorn:400, 400i, 700, 700i, 900, 900i',
-      'serif',
-      'Abril Fatface',
-      'cursive',
-      'UnifrakturCook',
-      'cursive',
+      "Rye",
+      "cursive",
+      "Vollkorn:400, 400i, 700, 700i, 900, 900i",
+      "serif",
+      "Abril Fatface",
+      "cursive",
+      "UnifrakturCook",
+      "cursive",
     ],
   },
 });
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
